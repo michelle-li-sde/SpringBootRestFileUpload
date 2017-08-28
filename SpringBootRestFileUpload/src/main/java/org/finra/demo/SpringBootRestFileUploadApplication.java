95d3cf43746c9a167c84cb5a200285529413e0c7
Michelle Li <michelle.li.sde@gmail.com>
2017-08-27 23:19:55 -0400

    new spring boot application

---
+++ SpringBootRestFileUpload/src/main/java/org/finra/demo/SpringBootRestFileUploadApplication.java
@@ -0,0 +1,12 @@
+package org.finra.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestFileUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestFileUploadApplication.class, args);
+	}
+}
